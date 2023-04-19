9f22f699217768815369f284bbeffa97537899dd
Giadagenovesi <giada.g@outlook.it>
2023-04-19 15:36:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}</script>
+
+<template>
+    <h1> Ciao</h1>
+    <i class="fa-regular fa-down-long"></i>
+</template>
+
+<style lang="scss">
+    @use "./styles/general.scss";
+</style>
