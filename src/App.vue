348cf4536d4ccece4c97d990e936e70fa07d0e27
Giadagenovesi <giada.g@outlook.it>
2023-04-20 11:34:21 +0200

    Completed MILESTONE 1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
-
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 import axios from "axios";
 import {store} from "./store";
 
 export default {
-    components: { AppHeader },
+    components: { AppHeader, AppMain },
     data() {
         return {
             store
@@ -13,23 +13,27 @@ export default {
     },
     methods: {
         handleSearch () {
+            this.getMovies();
+        },
+        getMovies() {
             axios.get (this.store.apiMovieURL, {
                 params: {
-                    apy_key: this.store.apiKey,
+                    api_key: this.store.apiKey,
                     query: this.store.searchedTitle,
                 }
             }).then((resp) => {
                 console.log(resp);
-                this.store.movies = resp.data.results
+                this.store.movies = resp.data.results,
+                this.store.searchedTitle = ""
             })
         }
     } 
-}
-  
+} 
 </script>
 
 <template>
     <AppHeader @search="handleSearch" />
+    <AppMain />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store';
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Film</h2>
+    <ul>
+        <li v-for="movie in store.movies">
+            <h4>{{ movie.title }} </h4>
+            <h5>{{ movie.original_title }} </h5>
+            <h6>{{ movie.original_language }}</h6>
+            <p>{{ movie.vote_average }}</p>
+        </li>
+    </ul>
+</template>
+
+<style>
+</style>
